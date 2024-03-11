332a78c941138e35b33ecf23480c74aec56bd380
Max Hösel <git@maxhoesel.de>
2024-03-11 19:37:27 +0100

    simplify s3 permissions

--- main.tf
+++ main.tf
@@ -15,14 +15,7 @@ resource "minio_iam_policy" "access_bucket" {
       {
         "Effect" : "Allow",
         "Action" : [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:GetObjectAcl",
-          "s3:PutObjectAcl",
-          "s3:ListBucket",
-          "s3:GetBucketLocation",
-          "s3:GetBucketPolicy"
+          "s3:*"
         ],
         "Resource" : [
           "arn:aws:s3:::${local.bucket_name}/*",
